068176742120eb48d653b08854ea299e69f47164
Dubaguntla Sandeep <109743819+Sandeepdubaguntla@users.noreply.github.com>
2025-07-14 10:36:52 +0530

    Add files via upload

---
+++ PatternPrograms/SandGlassPatterns/SandGlassPatternWithPalindromeAlphabets.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+/*
+	Pattern:
+
+		     a b c d c b a
+			   a b c b a
+			     a b a
+			       a
+			     a b a
+			   a b c b a
+			 a b c d c b a
+
+*/
+class SandGlassPatternWithPalindromeAlphabets 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter no of rows");
+		int row=sc.nextInt();
+		int star=row;
+		int space=0;
+		for (int i=1;i<=row ;i++ )
+		{
+			char ch='a';
+			for (int j=1;j<=space ;j++ )
+			{
+				System.out.print(" "+" ");
+			}
+			for (int k=1;k<=star ;k++ )
+			{
+				if (k<=star/2)
+				{
+					System.out.print(ch+" ");
+					ch++;
+				}
+				else
+				{
+					System.out.print(ch+" ");
+					ch--;
+				}
+			}
+			if (i<=row/2)
+			{
+				space++;
+				star=star-2;
+			}
+			else
+			{
+				space--;
+				star=star+2;
+			}
+			System.out.println();
+		}
+	}
+}
